8912f8325c593604940d2756204bdbfcc9bb363d
Aditya Oberai <adityaoberai1@gmail.com>
2024-06-17 12:25:59 +0000

    Update function to store state

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
-# language-translator-twilio-function
+# Twilio Challenge: Language Translator via Twilio Functions, WhatsApp, Appwrite & OpenAI
+
+## Description
 Twilio function that translates language using GPT-4o and responds through the Twilio Programmable Messaging on WhatsApp
 
 https://github.com/adityaoberai/language-translator-twilio-function/assets/31401437/7e042bcd-72f5-46aa-9de4-dad5dcb2aba1
+
+## How it works
+
+- **Twilio Programmable Messaging (WhatsApp Sandbox)** and **Twilio Functions** is used to communicate with the user
+- **Appwrite Database** is used to store the state of the user through the conversation, and all conversation data is deleted as soon as the translation is sent to the user
+- **OpenAI's GPT-4o API** is used to translate the text to the specified language
--- twilio-function.js
+++ twilio-function.js
@@ -1,6 +1,19 @@
 exports.handler = async function(context, event, callback) {
+    // Getting authenticated Twilio Client
     const twilioClient = context.getTwilioClient();
 
+    //Setting up Appwrite Client
+    const appwriteSdk = require('node-appwrite');
+    const appwriteClient = new appwriteSdk.Client();
+    appwriteClient
+        .setEndpoint(context.APPWRITE_ENDPOINT)
+        .setProject(context.APPWRITE_PROJECT_ID)
+        .setKey('<APPWRITE_API_KEY>'); // Was unable to use the Twilio Functions's Environment Variables for this one because of the set max length of 255 characters
+    const db = new appwriteSdk.Databases(appwriteClient);
+    const dbId = context.APPWRITE_DB_ID;
+    const collectionId = context.APPWRITE_COLLECTION_ID;
+
+    // Setting up OpenAI Client
     const OpenAI = require('openai');
     const openai = new OpenAI({
         apiKey: context.OPENAI_API_KEY
@@ -8,35 +21,106 @@ exports.handler = async function(context, event, callback) {
 
     const twiml = new Twilio.twiml.MessagingResponse();
 
-    const incomingMsg = event.Body.trim();
+    let incomingMessage = event.Body.trim();
+    let currentPhoneNumber = event.From.substring(9);
 
-    if(incomingMsg == "Hello Twilio") {
+    // Setting currentUser to help retain state of conversation
+    let currentUser = await db.listDocuments(
+        dbId,
+        collectionId,
+        [
+            appwriteSdk.Query.equal('phoneNumber', currentPhoneNumber)
+        ]
+    );
+    if(currentUser.total == 1) {
+        currentUser = currentUser.documents[0];
+    } 
+    else {
+        currentUser = await db.createDocument(
+            dbId,
+            collectionId,
+            appwriteSdk.ID.unique(),
+            {
+                phoneNumber: currentPhoneNumber,
+                lang: null,
+                text: null
+            }
+        );
+    }
+
+    if(!currentUser.text && !currentUser.lang) {
+        if(incomingMessage == 'Hello Twilio' || incomingMessage == 'yes') {
+            // Introductory message
+            twiml.message('Hi! Welcome to the Language Translator.\n\nPlease enter the language you want to translate to.');
+            return callback(null, twiml);
+        }
+
+        // Save target language
+        currentUser = await db.updateDocument(
+            dbId,
+            collectionId,
+            currentUser.$id,
+            {
+                phoneNumber: currentPhoneNumber,
+                lang: incomingMessage,
+                text: null
+            }
+        )
+        twiml.message('Please enter the text you want to translate.');
+        return callback(null, twiml);
+        
+    }
+    
+    else {
+        // Save text to translate
+        currentUser = await db.updateDocument(
+            dbId,
+            collectionId,
+            currentUser.$id,
+            {
+                phoneNumber: currentPhoneNumber,
+                lang: currentUser.lang,
+                text: incomingMessage
+            }
+        )
+    
+        const response = await openai.chat.completions.create({
+            model: 'gpt-4o',
+            messages: [
+                {
+                    role: "system",
+                    content: "You are a language translation assistant.",
+                },
+                { 
+                    role: "user", 
+                    content: `Translate the following text to ${currentUser.lang}: ${incomingMessage}\n\nReturn only the translated text in the response with no additional content surrounding it.`
+                }
+            ]
+        });
+
+        const translatedText = response.choices[0].message.content.trim();
+
+        // Send translated text to user
         await twilioClient.messages.create({
-            body: `Hi! Welcome to the Language Translator.\n\nPlease enter the text and language you want to translate to in the format as follows:`,
+            body: 'Here is the translated text',
             from: 'whatsapp:+14155238886', // Your Twilio Sandbox number
             to: event.From,
         });
-        twiml.message('Text to translate:\n\nLanguage to translate to:');
+
+        await twilioClient.messages.create({
+            body: translatedText,
+            from: 'whatsapp:+14155238886', // Your Twilio Sandbox number
+            to: event.From,
+        });
+
+        // Clear user state from Appwrite
+        await db.deleteDocument(
+            dbId,
+            collectionId,
+            currentUser.$id
+        );
+
+        twiml.message('If you would like to translate another text, please enter: yes');
         return callback(null, twiml);
     }
-
-    const response = await openai.chat.completions.create({
-        model: 'gpt-4o',
-        messages: [
-            {
-                role: "system",
-                content: "You are a language translation assistant.",
-            },
-            { 
-                role: "user", 
-                content: `Translate the following text using the following info: ${incomingMsg}\n\nReturn the response in the following format: "Translated Text: [TRANSLATED TEXT]"`
-            }
-        ]
-    });
-
-    const translatedText = response.choices[0].message.content.trim();
-
-    twiml.message(translatedText);
-
-    return callback(null, twiml);
 };
